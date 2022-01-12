0424a700865b266af375b54698e1d0f84ff8e0c6
Renato de Souza <renbstux@gmail.com>
2022-01-12 10:06:27 -0300

    Domain model, database seed

--- backend/src/main/java/com/devsuperior/dsmovie/config/SecurityConfig.java
+++ backend/src/main/java/com/devsuperior/dsmovie/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.devsuperior.dsmovie.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
